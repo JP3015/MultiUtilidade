2f7f47ad59d1f42253a0c6ce174ad20d063292dc
JP3015 <RM87894@fiap.com.br>
2023-01-27 14:01:08 -0300

    Alteração na página IP

--- src/pages/Ip/Ip.jsx
+++ src/pages/Ip/Ip.jsx
@@ -1,21 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import './Ip.css'
 
-
 export default function Ip(){
-    return(
-        <html>
+    const [details, setDetails] = useState(null);
+
+    const getUserGeolocationDetails = () => {
+        fetch(
+            "https://geolocation-db.com/json/0f761a30-fe14-11e9-b59f-e53803842572"
+        )
+            .then(response => response.json())
+            .then(data => setDetails(data));
+    };
+
+    return (
+        <>
             <Header />
             <body>
-                <main >
-                    <div>
-                       <p>Teste</p> 
-                    </div>
-                </main>
+                <div className="row">
+                    <h2>Achar endereço de Ip</h2>
+                    <p className="mt-3">
+                        <button
+                            className="btn btn-primary"
+                            onClick={getUserGeolocationDetails}>
+                            Clique aqui
+                        </button>
+                        <div className="row justify-content-center mt-3">
+                            <div className="col-lg-6 text-center text-dark">
+                                {details && (
+                                    <ul className="list-group">
+                                        <li className="list-group-item">
+                                            Localização :{" "}
+                                            {`${details.city}, ${details.country_name}(${details.country_code})`}
+                                        </li>
+                                        <li className="list-group-item">
+                                            IP: {details.IPv4}
+                                        </li>
+                                    </ul>
+                                )}
+                            </div>
+                        </div>
+                    </p>
+                </div>
             </body>
             <Footer />
-        </html>
-    )
-}
+        </>
+    );
+};
